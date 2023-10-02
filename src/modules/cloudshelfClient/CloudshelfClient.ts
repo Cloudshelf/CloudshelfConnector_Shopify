262b03d9a527b8c82155a63f7c8739f8204e473f
Ashley Williams <ashleyww93@gmail.com>
2023-10-02 15:11:50 +0100

    Update CloudshelfClient.ts

--- src/modules/cloudshelfClient/CloudshelfClient.ts
+++ src/modules/cloudshelfClient/CloudshelfClient.ts
@@ -10,37 +10,29 @@ import { createHmac } from "../../utils/hmac";
 
 export class CloudshelfClientFactory {
   public static getClient(domain?: string) {
-    const timestamp = new Date().getTime().toString();
-    const hmac = domain ? createHmac(domain, timestamp) : "";
-
     const httpLink = createHttpLink({
       uri: process.env.CLOUDSHELF_API_URL!,
-      headers: {
-        ...(domain
-          ? { "x-store-domain": domain, "x-hmac": hmac, "x-nonce": timestamp }
-          : {}),
-      },
     });
-    //
-    // const authLink = new ApolloLink((operation, forward) => {
-    //   const timestamp = new Date().getTime().toString();
-    //   const hmac = domain ? createHmac(domain, timestamp) : "";
-    //
-    //   operation.setContext(({ headers = {} }) => ({
-    //     headers: {
-    //       ...headers,
-    //       ...(domain
-    //         ? { "x-store-domain": domain, "x-hmac": hmac, "x-nonce": timestamp }
-    //         : {}),
-    //     },
-    //   }));
-    //
-    //   return forward(operation);
-    // });
+
+    const authLink = new ApolloLink((operation, forward) => {
+      const timestamp = new Date().getTime().toString();
+      const hmac = domain ? createHmac(domain, timestamp) : "";
+
+      operation.setContext(({ headers = {} }) => ({
+        headers: {
+          ...headers,
+          ...(domain
+            ? { "x-store-domain": domain, "x-hmac": hmac, "x-nonce": timestamp }
+            : {}),
+        },
+      }));
+
+      return forward(operation);
+    });
 
     return new ApolloClient({
       cache: new InMemoryCache(),
-      link: from([httpLink]), //authLink
+      link: from([authLink, httpLink]),
     });
   }
 }

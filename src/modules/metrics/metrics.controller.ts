02b213c4f516b747861e1a5668774a939a9e35bb
Ashley Williams <ashleyww93@gmail.com>
2023-12-27 16:57:31 +0000

    chore: add sentry to health check

--- src/modules/metrics/metrics.controller.ts
+++ src/modules/metrics/metrics.controller.ts
@@ -2,6 +2,7 @@ import { Controller, Get } from "routing-controllers";
 import { QueueNames } from "../queue/queue.names.const";
 import { Container } from "../../container";
 import { createProductGroupTriggerJob } from "../queue/queues/productgroup/productgroup.job.functions";
+import * as Sentry from "@sentry/node";
 
 @Controller("/metrics")
 export class MetricsController {
@@ -13,6 +14,13 @@ export class MetricsController {
   @Get("/health")
   async health() {
     const queueNames = Object.keys(QueueNames);
+    Sentry.startTransaction({
+      op: "Noble:HealthCheck",
+      name: "Noble Health Check",
+      data: {
+        queueNames: queueNames.join(", "),
+      },
+    }).finish();
 
     for (const queueName of queueNames) {
       await Container.queueService.checkQueueHealth(queueName);
